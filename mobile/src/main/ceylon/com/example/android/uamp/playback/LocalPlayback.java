14ba5967fd61a48561cd8a2f60adcda740618cdd
gavinking <gavin@ceylon-lang.org>
2017-03-13 11:20:30 +0100

    very minor

--- mobile/src/main/ceylon/com/example/android/uamp/playback/LocalPlayback.java
+++ mobile/src/main/ceylon/com/example/android/uamp/playback/LocalPlayback.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.uamp.playback;
+//package com.example.android.uamp.playback;
 //
 //import android.content.BroadcastReceiver;
 //import android.content.Context;
